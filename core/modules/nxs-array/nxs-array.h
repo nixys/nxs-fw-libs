3b41f8c919f9a605c2f62a7e876980ad19308951
Anna Kamenskaia <ladymacbeth94@yandex.ru>
2017-06-10 17:45:52 +0700

    Some rework on nxs-array

--- core/modules/nxs-array/nxs-array.h
+++ core/modules/nxs-array/nxs-array.h
@@ -5,6 +5,12 @@
  *
  * @brief Module 'nxs-array' is a basic module providing functions to work with arrays.
  *
+ * In Nixys Framework an array is a pointer to a block of memory. Elements are distinguished by
+ * setting nxs_array_s#size which defines amount of memory allocated for a single element.
+ *
+ * The size of the array itself can be counted as nxs_array_s#nalloc * nxs_array_s size.
+ *
+ * The length of an array is defined by nxs_array_s#count. 
  *  @{
  */
 
@@ -18,7 +24,7 @@ struct nxs_array_s
 	size_t		size; /**< size of an element stored in array in bytes*/
 	u_int		count; /**< number of elements stored in array*/
 	u_int		step; /**< a number by which the nxs_array_nalloc can be extended at once*/
-	void		*el; /**< pointer to the data array */
+	void		*el; /**< pointer to the data*/
 };
 
 nxs_array_t		*nxs_array_malloc		(u_int nalloc, size_t size, u_int step);
@@ -36,16 +42,20 @@ nxs_array_t		*nxs_array_malloc		(u_int nalloc, size_t size, u_int step);
 void			*nxs_array_destroy		(nxs_array_t *array);
 
 /**
+ * @brief Initializes an array by allocating \b size * \b nalloc bytes of memory set to zero.
+ *
  * @param array Pointer to the array.
  * @param nalloc Maximum number of elements for the array.
- * @param size
+ * @param size Amount of memory allocated for a single element of the array.
+ * @param step A value by which the size of the array is extended when it's necessary.
+ *
  * @return
  * * none
  */
 void			nxs_array_init			(nxs_array_t *array, u_int nalloc, size_t size, u_int step);
 
 /**
- * @brief Deallocates memory for an array and sets the number of its elements to 0.
+ * @brief Deallocates memory for the data stored in an array and sets the number of its elements to 0.
  *
  * @param array Pointer to the array.
  *
@@ -55,7 +65,9 @@ void			nxs_array_init			(nxs_array_t *array, u_int nalloc, size_t size, u_int st
 void			nxs_array_free			(nxs_array_t *array);
 
 /**
- * @brief Clears an array by setting its nxs_array_s#count to 0
+ * @brief Clears an array by setting its nxs_array_s#count to 0.
+ *
+ * As a result the old data will be rewritten when new elements are added.
  *
  * @param array Pointer to the array
  *
@@ -65,9 +77,9 @@ void			nxs_array_free			(nxs_array_t *array);
 void			nxs_array_clear			(nxs_array_t *array);
 
 /**
- * @brief Adds an element to the end of the array.
+ * @brief Adds an element to the end of an array.
  *
- * If new element exceeds nxs_array_s#nalloc, additional memory will be allocated for it.
+ * If the new element exceeds nxs_array_s#nalloc, additional memory will be allocated for it.
  *
  * The memory allocated for the element is filled with zeroes.
  *
@@ -93,7 +105,7 @@ void			*nxs_array_add			(nxs_array_t *array);
 void			*nxs_array_add_i		(nxs_array_t *array, u_int i);
 
 /**
- * @brief Return an element at the specified index of an array.
+ * @brief Gets an element at the specified index of an array.
  *
  * @param array Pointer to the array.
  * @param i Index of the element.
@@ -127,7 +139,7 @@ u_int			nxs_array_nalloc		(nxs_array_t *array);
 u_int			nxs_array_count			(nxs_array_t *array);
 
 /**
- * @brief Returns the size of the elements of an array.
+ * @brief Returns the amount of memory allocated for a single element of an array.
  *
  * @param array Pointer to the array.
  *
@@ -140,7 +152,7 @@ u_int			nxs_array_size			(nxs_array_t *array);
 /**
  * @brief Returns the value of nxs_array_s#step for an array.
  *
- * nxs_array_s#step is the number of allocated elements when the array is being extended.
+ * nxs_array_s#step defines how much more elements can be stored in an array once it is extended.
  *
  * @param array Pointer to the array.
  *
