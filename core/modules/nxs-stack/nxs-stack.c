5db8127873a068ab92e19271bfc74cf8b7f0db7b
Boris Ershov <b.ershov@nixys.ru>
2017-09-01 16:01:04 +0700

    Added new functions (to get elements count and size) to 'nxs-stack'

--- core/modules/nxs-stack/nxs-stack.c
+++ core/modules/nxs-stack/nxs-stack.c
@@ -1,8 +1,36 @@
+// clang-format off
+
+/* Module includes */
+
 #include <nxs-core/nxs-core.h>
 
-nxs_stack_t *nxs_stack_malloc(u_int size)
+/* Module definitions */
+
+
+
+/* Module typedefs */
+
+
+
+/* Module declarations */
+
+
+
+/* Module internal (static) functions prototypes */
+
+// clang-format on
+
+// clang-format off
+
+/* Module initializations */
+
+/* Module global functions */
+
+// clang-format on
+
+nxs_stack_t *nxs_stack_malloc(size_t size)
 {
-	nxs_stack_t	*stack = NULL;
+	nxs_stack_t *stack = NULL;
 
 	stack = (nxs_stack_t *)nxs_malloc(stack, sizeof(nxs_stack_t));
 
@@ -14,7 +42,7 @@ nxs_stack_t *nxs_stack_malloc(u_int size)
 nxs_stack_t *nxs_stack_destroy(nxs_stack_t *stack)
 {
 
-	if(stack == NULL){
+	if(stack == NULL) {
 
 		return NULL;
 	}
@@ -24,28 +52,34 @@ nxs_stack_t *nxs_stack_destroy(nxs_stack_t *stack)
 	return nxs_free(stack);
 }
 
-void nxs_stack_init(nxs_stack_t *stack, u_int size)
+void nxs_stack_init(nxs_stack_t *stack, size_t size)
 {
 
-	if(stack == NULL){
+	if(stack == NULL) {
 
 		return;
 	}
 
 	stack->count = 0;
-	stack->head = NULL;
-	stack->size = size;
+	stack->head  = NULL;
+	stack->size  = size;
 }
 
 void nxs_stack_free(nxs_stack_t *stack)
 {
 
-	while(nxs_stack_pull(stack));
+	while(nxs_stack_pull(stack)) {
+	};
 }
 
 void *nxs_stack_push(nxs_stack_t *stack)
 {
 	nxs_stack_el_t *p = NULL;
+
+	if(stack == NULL) {
+
+		return NULL;
+	}
 
 	p = (nxs_stack_el_t *)nxs_calloc(p, sizeof(nxs_stack_el_t));
 
@@ -64,12 +98,12 @@ void *nxs_stack_pull(nxs_stack_t *stack)
 {
 	nxs_stack_el_t *p;
 
-	if(stack == NULL){
+	if(stack == NULL) {
 
 		return NULL;
 	}
 
-	if(stack->head == NULL){
+	if(stack->head == NULL) {
 
 		return NULL;
 	}
@@ -77,24 +111,53 @@ void *nxs_stack_pull(nxs_stack_t *stack)
 	p = stack->head->next;
 
 	stack->head->data = nxs_free(stack->head->data);
-	stack->head = nxs_free(stack->head);
+	stack->head       = nxs_free(stack->head);
 
 	stack->head = p;
 
 	stack->count--;
 
-	if(p == NULL){
+	if(p == NULL) {
 
 		return NULL;
 	}
-	else{
+	else {
 
 		return p->data;
 	}
 }
 
+size_t nxs_stack_count(nxs_stack_t *stack)
+{
+
+	if(stack == NULL) {
+
+		return 0;
+	}
+
+	return stack->count;
+}
+
+size_t nxs_stack_size(nxs_stack_t *stack)
+{
+
+	if(stack == NULL) {
+
+		return 0;
+	}
+
+	return stack->size;
+}
+
 void *nxs_stack_get(nxs_stack_t *stack)
 {
 
+	if(stack == NULL) {
+
+		return NULL;
+	}
+
 	return stack->head->data;
 }
+
+/* Module internal (static) functions */
--- core/modules/nxs-stack/nxs-stack.h
+++ core/modules/nxs-stack/nxs-stack.h
@@ -1,28 +1,40 @@
 #ifndef _INCLUDE_NXS_STACK_H
 #define _INCLUDE_NXS_STACK_H
 
+// clang-format off
+
+/** @addtogroup nxs-stack
+ *
+ * @brief Module 'nxs-stack' is a basic module providing functions to work with memory stack.
+ *
+ *  @{
+ */
+
 #include <nxs-core/nxs-core.h>
 
 struct nxs_stack_el_s
 {
-	void					*data;
+	void			*data;
 	struct nxs_stack_el_s	*next;
 };
 
 struct nxs_stack_s
 {
-	nxs_stack_el_t			*head;
+	nxs_stack_el_t		*head;
 
-	int						count;
-	u_int					size;
+	size_t			count;
+	size_t			size;
 };
 
-nxs_stack_t			*nxs_stack_malloc				(u_int size);
-nxs_stack_t			*nxs_stack_destroy				(nxs_stack_t *stack);
-void				nxs_stack_init					(nxs_stack_t *stack, u_int size);
-void				nxs_stack_free					(nxs_stack_t *stack);
-void				*nxs_stack_push					(nxs_stack_t *stack);
-void				*nxs_stack_pull					(nxs_stack_t *stack);
-void				*nxs_stack_get					(nxs_stack_t *stack);
+nxs_stack_t			*nxs_stack_malloc			(size_t size);
+nxs_stack_t			*nxs_stack_destroy			(nxs_stack_t *stack);
+void				nxs_stack_init				(nxs_stack_t *stack, size_t size);
+void				nxs_stack_free				(nxs_stack_t *stack);
+void				*nxs_stack_push				(nxs_stack_t *stack);
+void				*nxs_stack_pull				(nxs_stack_t *stack);
+size_t				nxs_stack_count				(nxs_stack_t *stack);
+size_t				nxs_stack_size				(nxs_stack_t *stack);
+void				*nxs_stack_get				(nxs_stack_t *stack);
 
+/** @} */ // end of nxs-fsstack
 #endif /* _INCLUDE_NXS_STACK_H */
